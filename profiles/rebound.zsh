bb3b2c4957911692389bf4560e5b4b6a1e0e97f7
Jimmie Hogklint <hogklint@gmail.com>
2018-10-05 21:14:20 +0200

    Add rust bins and path

--- profiles/rebound.zsh
+++ profiles/rebound.zsh
@@ -8,3 +8,13 @@ export prompt_color="${fg_green}"
 #zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==02=01}:${(s.:.)LS_COLORS}")'
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/llvm
+
+if [ -d "/home/hogklint/.cargo/bin" ]
+then
+  export PATH="$PATH:/home/hogklint/.cargo/bin"
+fi
+
+if [ -d "$(rustc --print sysroot)/lib/rustlib/src/rust/src" ]
+then
+  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+fi
