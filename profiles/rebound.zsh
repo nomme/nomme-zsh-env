e4ab562837e935796c4d4b1dfdb93feb17c6e6b4
Jimmie Hogklint <hogklint@gmail.com>
2018-11-17 12:40:00 +0100

    Disable rustc init on rebound
    Delays shell init quite a bit.

--- profiles/rebound.zsh
+++ profiles/rebound.zsh
@@ -12,12 +12,14 @@ export prompt_color="${fg_green}"
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/llvm
 
-if [ -d "/home/hogklint/.cargo/bin" ]
-then
-  export PATH="$PATH:/home/hogklint/.cargo/bin"
-fi
 
-if [ -d "$(rustc --print sysroot)/lib/rustlib/src/rust/src" ]
-then
-  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-fi
+#Temp commented because delay when opening shell
+#if [ -d "/home/hogklint/.cargo/bin" ]
+#then
+#  export PATH="$PATH:/home/hogklint/.cargo/bin"
+#fi
+
+#if [ -d "$(rustc --print sysroot)/lib/rustlib/src/rust/src" ]
+#then
+#  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+#fi
