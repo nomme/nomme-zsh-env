ba737e9442908939cb5b5cd4849c5ee36c6f448c
Jimmie Hogklint <jimmie.hogklint@aptiv.com>
2018-12-20 08:18:41 +0100

    Perl paths

--- profiles/dawn.zsh
+++ profiles/dawn.zsh
@@ -6,6 +6,12 @@ export FZF_DEFAULT_COMMAND='ag -g "" --ignore "*Test.[ch]pp" --ignore "*\.$"'
 
 export USE_CCACHE="Y"
 
+PATH="/home/jimmieh/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/home/jimmieh/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/home/jimmieh/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/home/jimmieh/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/home/jimmieh/perl5"; export PERL_MM_OPT;
+
 if [ -d "$HOME/local/tmuxifier/bin" ];
 then
   export PATH="$HOME/local/tmuxifier/bin:$PATH"
